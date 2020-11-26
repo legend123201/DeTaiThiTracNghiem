df49e74e034d512bb4d2dff4f704dce5d9d0d7b7
legend123201 <legendofall18@gmail.com>
2020-11-26 23:47:05 +0700

    a

---
+++ README.md
@@ -0,0 +1,2 @@
+# DeTaiThiTracNghiem
+ 
---
+++ THITRACNGHIEM/THITRACNGHIEM/frmDangNhap.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace THITRACNGHIEM
+{
+    public partial class frmDangNhap : Form
+    {
+        public frmDangNhap()
+        {
+            InitializeComponent();
+        }
+
+        private void frmDangNhap_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'ds_V_DSPM.v_DS_PHANMANH' table. You can move, or remove it, as needed.
+            this.v_DS_PHANMANHTableAdapter.Fill(this.ds_V_DSPM.v_DS_PHANMANH);
+            cmb_COSO.SelectedIndex = 1; cmb_COSO.SelectedIndex = 0;
+        }
+
+        private void btnDN_Click(object sender, EventArgs e)
+        {
+            if (txtLogin.Text.Trim() == "" || txtPassword.Text.Trim() == "")
+            {
+                MessageBox.Show("Login name và mật mã không được trống", "", MessageBoxButtons.OK);
+                return;
+            }
+            Program.mlogin = txtLogin.Text; Program.password = txtPassword.Text;
+            if (Program.KetNoi() == 0) return;
+
+            Program.mCoso = cmb_COSO.SelectedIndex;
+
+            Program.mloginDN = Program.mlogin;
+            Program.passwordDN = Program.password;
+            string strLenh = "EXEC SP_THONGTINDANGNHAP '" + Program.mlogin + "'";
+
+            Program.myReader = Program.ExecSqlDataReader(strLenh);
+            if (Program.myReader == null) return;
+            Program.myReader.Read();
+
+
+            Program.username = Program.myReader.GetString(0);     // Lay user name
+            if (Convert.IsDBNull(Program.username))
+            {
+                MessageBox.Show("Login bạn nhập không có quyền truy cập dữ liệu\n Bạn xem lại username, password", "", MessageBoxButtons.OK);
+                return;
+            }
+            Program.mHoten = Program.myReader.GetString(1);
+            Program.mGroup = Program.myReader.GetString(2);
+            Program.myReader.Close();
+            Program.conn.Close();
+
+            Program.frmChinh.MaNV.Text = Program.username;
+            Program.frmChinh.HoTen.Text = Program.mHoten;
+            Program.frmChinh.Nhom.Text = Program.mGroup;
+
+            MessageBox.Show("Nhân viên - Nhóm : " + Program.mHoten + " - " + Program.mGroup, "", MessageBoxButtons.OK);
+        }
+
+        private void cmb_COSO_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (cmb_COSO.SelectedValue != null) //vì 1 lí do nào đó mà cái combo nó tự động đưa giá trị về null khi bấm btnDangnhap
+                    Program.servername = cmb_COSO.SelectedValue.ToString();
+
+            }
+            catch (Exception)
+            {
+            };
+        }
+    }
+}
